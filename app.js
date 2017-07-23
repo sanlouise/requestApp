22caa2bea780b99f825deec5738c5de28f52523a
sanlouise <sandra.adamshallie@gmail.com>
2017-07-23 11:13:36 -0400

    Create express server, listen for google.com

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-let express = require('express');
-let request = require('request');
-let fs = require('fs');
-let app = express();
-let port = 8080;
+const express = require('express');
+const request = require('request');
+const fs = require('fs');
+const app = express();
+const port = 8080;
+
+const url = "http://google.com";
+request(url, function(err, resp, body){
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(body);
+  }
+})
 
 app.listen(port, function() {
   console.log('app listening on port 8080')
