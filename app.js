9f8d6a0638ac78c373cc86a865a4331bb85b674b
sanlouise <sandra.adamshallie@gmail.com>
2017-07-23 11:30:25 -0400

    Update app.js to download google.com into folder

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const request = require('request');
 const fs = require('fs');
 const app = express();
 const port = 8080;
-
 const url = "http://google.com";
-request(url, function(err, resp, body){
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(body);
-  }
-})
+
+//EXAMPLE 1, log the page in the console
+// request(url, function(err, resp, body){
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(body);
+//   }
+// })
+
+//EXAMPLE 2, download the page
+const destination = fs.createWriteStream('./downloads/google2.html')
+request(url).pipe(destination)
 
 app.listen(port, function() {
   console.log('app listening on port 8080')
